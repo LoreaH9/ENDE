65e70aaeed66de43b29d41b71c09d6c6d96c51c3
Lorea <Lorea@DESKTOP-980UV38.mshome.net>
2020-11-17 10:25:30 +0100

    //menu 4 opciones
    //opciones

---
+++ MenuakGit/src/Menuak.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Menuak {
+	
+	public static void main(String[] args) {
+		Scanner entrada= new Scanner (System.in);
+		System.out.println(""
+				+ "********************************\r\n"
+				+ "Menu: \r\n"
+				+ "1. + \r\n"
+				+ "2. - \r\n"
+				+ "3. * \r\n"
+				+ "4. / \r\n"
+				+ "********************************");
+		System.out.println("Mete los dos numeros");
+		int x=entrada.nextInt(), y=entrada.nextInt();
+		System.out.println("Que operacion quieres hacer");
+		String aukera=entrada.next();
+		switch (aukera) {
+		case "+":
+			System.out.print(x+y);break;
+		case "-":
+			System.out.print(x-y);break;
+		case "*":
+			System.out.print(x*y);break;
+		case "/":
+			System.out.print(x/y);break;
+		default:
+			System.out.println("no has metido un valor válido");
+			aukera=entrada.next();
+		}
+
+	}
+	
+}
