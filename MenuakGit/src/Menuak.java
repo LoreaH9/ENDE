7e02f4445dfb79b848e54c119938da3d663db6a3
Lorea <Lorea@DESKTOP-980UV38.mshome.net>
2020-11-17 10:45:11 +0100

    //fin implementación

--- MenuakGit/src/Menuak.java
+++ MenuakGit/src/Menuak.java
@@ -1,7 +1,26 @@
 //ariketa
 import java.util.*;
 public class Menuak {
-	
+	public static void suma(int z1, int z2) {
+		System.out.print(z1+z2);
+	}
+	public static void resta(int z1, int z2) {
+		System.out.print(z1-z2);
+	}
+	public static void mult(int z1, int z2) {
+		System.out.print(z1*z2);
+	}
+	public static void div(int z1, int z2) {
+		System.out.print(z1/z2);
+	}
+	public static void fact(int z1) {
+		int i=1,fact=1;
+		while (i<=z1) {
+			fact=i*fact;
+			i++;
+		}
+		System.out.println(fact);
+	}
 	public static void main(String[] args) {
 		Scanner entrada= new Scanner (System.in);
 		System.out.println(""
@@ -20,31 +39,26 @@ public class Menuak {
 		case "+":
 			System.out.println("Mete los dos numeros");
 			int x=entrada.nextInt(), y=entrada.nextInt();
-			System.out.print(x+y);break;
+			suma(x,y);break;
 		case "-":
 			System.out.println("Mete los dos numeros");
 			x=entrada.nextInt();
 			y=entrada.nextInt();			
-			System.out.print(x-y);break;
+			resta (x,y);break;
 		case "*":
 			System.out.println("Mete los dos numeros");
 			x=entrada.nextInt();
 			y=entrada.nextInt();			
-			System.out.print(x*y);break;
+			mult(x,y);break;
 		case "/":
 			System.out.println("Mete los dos numeros");
 			x=entrada.nextInt();
 			y=entrada.nextInt();
-			System.out.print(x/y);break;
+			div(x,y);break;
 		case "factorial":
 			System.out.println("Mete el numeros");
 			x=entrada.nextInt();
-			int i=1,fact=1;
-			while (i<=x) {
-				fact=i*fact;
-				i++;
-			}
-			System.out.println(fact);break;
+			fact(x);break;
 		default:
 			System.out.println("no has metido un valor válido");
 			aukera=entrada.next();
