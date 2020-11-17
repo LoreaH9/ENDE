1d27ba5a1f5f0b8bc491fbae7036498c61fe87c0
Lorea <Lorea@DESKTOP-980UV38.mshome.net>
2020-11-17 10:41:20 +0100

    //menú 5 opciones implementado

--- MenuakGit/src/Menuak.java
+++ MenuakGit/src/Menuak.java
@@ -11,20 +11,40 @@ public class Menuak {
 				+ "2. - \r\n"
 				+ "3. * \r\n"
 				+ "4. / \r\n"
+				+ "5. factorial \r\n"
 				+ "********************************");
-		System.out.println("Mete los dos numeros");
-		int x=entrada.nextInt(), y=entrada.nextInt();
+		
 		System.out.println("Que operacion quieres hacer");
 		String aukera=entrada.next();
 		switch (aukera) {
 		case "+":
+			System.out.println("Mete los dos numeros");
+			int x=entrada.nextInt(), y=entrada.nextInt();
 			System.out.print(x+y);break;
 		case "-":
+			System.out.println("Mete los dos numeros");
+			x=entrada.nextInt();
+			y=entrada.nextInt();			
 			System.out.print(x-y);break;
 		case "*":
+			System.out.println("Mete los dos numeros");
+			x=entrada.nextInt();
+			y=entrada.nextInt();			
 			System.out.print(x*y);break;
 		case "/":
+			System.out.println("Mete los dos numeros");
+			x=entrada.nextInt();
+			y=entrada.nextInt();
 			System.out.print(x/y);break;
+		case "factorial":
+			System.out.println("Mete el numeros");
+			x=entrada.nextInt();
+			int i=1,fact=1;
+			while (i<=x) {
+				fact=i*fact;
+				i++;
+			}
+			System.out.println(fact);break;
 		default:
 			System.out.println("no has metido un valor válido");
 			aukera=entrada.next();
